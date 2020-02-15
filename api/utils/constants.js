d8ca89d464d9ee3903c42451ca5751bfa5d9c9e3
Emma Hughes <emma.setohughes@gmail.com>
2020-02-15 18:30:23 -0500

    add constants

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -1,5 +1,6 @@
 'use strict';
 
 exports.URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/book-party'
-exports.PORT = process.env.PORT || 3001;
+exports.PORT = process.env.PORT || 4000;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
+exports.PUBLIC_FOLDER = (process.env.NODE_ENV === 'production') ? 'build' : 'public'; 
